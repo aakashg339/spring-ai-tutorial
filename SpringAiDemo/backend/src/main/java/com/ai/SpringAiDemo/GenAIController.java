cabd02bff063acd241f46051eeb3a15cfa651bea
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-02-18 11:01:17 +0530

    feat(SpringAiDemo-backend): Added endpoint for image generation
    The endpoint uses the overloaded method of call without customisations.

--- SpringAiDemo/backend/src/main/java/com/ai/SpringAiDemo/GenAIController.java
+++ SpringAiDemo/backend/src/main/java/com/ai/SpringAiDemo/GenAIController.java
@@ -1,15 +1,23 @@
 package com.ai.SpringAiDemo;
 
+import java.io.IOException;
+
+import org.springframework.ai.image.ImageResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 public class GenAIController {
 
     @Autowired
     ChatService chatService;
+
+    @Autowired
+    ImageService imageService;
 
     @GetMapping("ask-ai")
     public String getResponse(@RequestParam String prompt) {
@@ -19,6 +27,13 @@ public class GenAIController {
     @GetMapping("ask-ai-options")
     public String getResponseOptions(@RequestParam String prompt) {
         return chatService.getResponseOptions(prompt);
+    }
+
+    @GetMapping("generate-image")
+    public void generateImages(HttpServletResponse response, @RequestParam String prompt) throws IOException {
+        ImageResponse imageResponse = imageService.generateImage(prompt);
+        String imageUrl = imageResponse.getResult().getOutput().getUrl();
+        response.sendRedirect(imageUrl);
     }
 
 }
