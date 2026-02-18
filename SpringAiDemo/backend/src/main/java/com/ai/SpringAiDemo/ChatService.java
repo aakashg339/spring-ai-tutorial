9cda110f13a5c949acbf0f2f8b50e78e01010433
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-02-18 07:46:14 +0530

    feat(SpringAiDemo-backend): Added service class to make a call to model

---
+++ SpringAiDemo/backend/src/main/java/com/ai/SpringAiDemo/ChatService.java
@@ -0,0 +1,17 @@
+package com.ai.SpringAiDemo;
+
+import org.springframework.ai.chat.model.ChatModel;
+
+public class ChatService {
+
+    private final ChatModel chatModel;
+
+    public ChatService(ChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    public String getResponse(String prompt) {
+        return chatModel.call(prompt);
+    }
+
+}
