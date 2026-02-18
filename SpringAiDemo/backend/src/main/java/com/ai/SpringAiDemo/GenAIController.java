01067e1e5f833be17cf0e83ee0f3770b97f2e254
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-02-18 08:08:02 +0530

    feat(SpringAiDemo-backend): Added controller to interact with the model using api

---
+++ SpringAiDemo/backend/src/main/java/com/ai/SpringAiDemo/GenAIController.java
@@ -0,0 +1,19 @@
+package com.ai.SpringAiDemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GenAIController {
+
+    @Autowired
+    ChatService chatService;
+
+    @GetMapping("ask-ai")
+    public String getResponse(@RequestParam String prompt) {
+        return chatService.getResponse(prompt);
+    }
+
+}
