fbccd7cf6ff5fcac879942e3f3e5635860b670c0
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-02-20 07:12:06 +0530

    feat(SpringAiDemo-frontend): Added three buttons
    Three buttons for image generationnnn, chat and recipe generation.

--- SpringAiDemo/frontend/src/App.jsx
+++ SpringAiDemo/frontend/src/App.jsx
@@ -4,31 +4,25 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [activeTab, setActiveTab] = useState('image-generator');
+
+  const handleTabChange = (tab) => {
+    alert(tab);
+    setActiveTab(tab);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <button onClick={() => handleTabChange('image-generator')}>
+        Image Generator
+      </button>
+      <button onClick={() => handleTabChange('chat')}>
+        Chat
+      </button>
+      <button onClick={() => handleTabChange('recipe-generator')}>
+        Recipe Generator
+      </button>
+    </div>
   )
 }
 
